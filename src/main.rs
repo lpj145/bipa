e519f4d4f7f1f832caa1bbf2bd4ad050e051e01c
Marcos Dantas <marcos.adantas@hotmail.com>
2024-09-25 11:18:06 -0300

    feat: starting http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Json, Router, http::StatusCode};
+mod ingestor;
+
+async fn list_nodes() -> &'static str {
+    "data"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(list_nodes));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
